781ff1c11ca266143eed5b8a59b2ca541630361b
w1la <w1la920@gmail.com>
2024-01-23 20:02:58 +0300

    k
    jk

---
+++ sandbox/src/main/java/CodeAbbeySumInLoop.java
@@ -0,0 +1,8 @@
+package section1_setup;
+
+public class CodeAbbeySumInLoop {
+    public static void main(String[] args) {
+        System.out.println("Hello,world!");
+    }
+    }
+
---
+++ sandbox/src/main/java/MyFirstProgram/src/MyFirstProframm.java
@@ -0,0 +1,5 @@
+public class MyFirstProframm {
+    public static void main(String[] args) {
+
+    }
+}
